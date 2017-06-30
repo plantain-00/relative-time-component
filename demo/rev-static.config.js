2727853ec7e6b7dbaebf9b5f0fbbef15c4072292
york yao <yaoao12306@outlook.com>
2017-06-30 22:33:51 +0800

    lint config js file

--- demo/rev-static.config.js
+++ demo/rev-static.config.js
@@ -1,13 +1,13 @@
 module.exports = {
-    inputFiles: [
-        "demo/**/index.bundle.js",
-        "demo/*.bundle.css",
-        "demo/**/index.ejs.html",
-    ],
-    outputFiles: file => file.replace(".ejs", ""),
-    ejsOptions: {
-        rmWhitespace: true
-    },
-    sha: 256,
-    customNewFileName: (filePath, fileString, md5String, baseName, extensionName) => baseName + "-" + md5String + extensionName,
-};
+  inputFiles: [
+    'demo/**/index.bundle.js',
+    'demo/*.bundle.css',
+    'demo/**/index.ejs.html'
+  ],
+  outputFiles: file => file.replace('.ejs', ''),
+  ejsOptions: {
+    rmWhitespace: true
+  },
+  sha: 256,
+  customNewFileName: (filePath, fileString, md5String, baseName, extensionName) => baseName + '-' + md5String + extensionName
+}
