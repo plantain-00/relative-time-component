cf6efac772f786b5e6d241c4a3910df219926632
york yao <yaoao12306@outlook.com>
2017-10-09 16:26:03 +0800

    remove screenshot source; change screenshot script to ts

--- clean-scripts.config.js
+++ clean-scripts.config.js
@@ -1,5 +1,6 @@
 const childProcess = require('child_process')
 const util = require('util')
+const { Service } = require('clean-scripts')
 
 const execAsync = util.promisify(childProcess.exec)
 
@@ -18,37 +19,7 @@ module.exports = {
       css: `cleancss -o demo/index.bundle.css ./node_modules/github-fork-ribbon-css/gh-fork-ribbon.css`,
       clean: `rimraf demo/**/index.bundle-*.js demo/*.bundle-*.css demo/**/*.index.bundle-*.js`
     },
-    `rev-static --config demo/rev-static.config.js`,
-    async () => {
-      const { createServer } = require('http-server')
-      const puppeteer = require('puppeteer')
-      const fs = require('fs')
-      const beautify = require('js-beautify').html
-      const parse5 = require('parse5')
-      const server = createServer()
-      server.listen(8000)
-      const browser = await puppeteer.launch()
-      const page = await browser.newPage()
-      await page.emulate({ viewport: { width: 1440, height: 900 }, userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36' })
-      for (const type of ['vue', 'react', 'angular']) {
-        await page.goto(`http://localhost:8000/demo/${type}`)
-        await page.waitFor(1000)
-        await page.screenshot({ path: `demo/${type}/screenshot.png`, fullPage: true })
-        const content = await page.content()
-        const document = parse5.parse(content)
-        forEach(document, node => {
-          if (node.attrs) {
-            const attr = node.attrs.find(a => a.name === 'title')
-            if (attr) {
-              attr.value = '[title]'
-            }
-          }
-        })
-        fs.writeFileSync(`demo/${type}/screenshot-src.html`, beautify(parse5.serialize(document)))
-      }
-      server.close()
-      browser.close()
-    }
+    `rev-static --config demo/rev-static.config.js`
   ],
   lint: {
     ts: `tslint "src/**/*.ts" "src/**/*.tsx" "demo/**/*.ts" "demo/**/*.tsx"`,
@@ -58,7 +29,6 @@ module.exports = {
   test: [
     'tsc -p spec',
     'karma start spec/karma.config.js',
-    'git checkout "demo/**/screenshot.png"',
     async () => {
       const { stdout } = await execAsync('git status -s')
       if (stdout) {
@@ -79,14 +49,10 @@ module.exports = {
     demo: `tsc -p demo --watch`,
     webpack: `webpack --config demo/webpack.config.js --watch`,
     rev: `rev-static --config demo/rev-static.config.js --watch`
-  }
-}
-
-function forEach (node, callback) {
-  callback(node)
-  if (node.childNodes) {
-    for (const childNode of node.childNodes) {
-      forEach(childNode, callback)
-    }
-  }
+  },
+  screenshot: [
+    new Service(`http-server -p 8000`),
+    `tsc -p screenshots`,
+    `node screenshots/index.js`
+  ]
 }
