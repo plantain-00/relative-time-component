b46b186179e896735328001a4e2db6db60a89ae2
york yao <yaoao12306@outlook.com>
2018-02-27 08:08:34 +0800

    build: improve scripts

--- clean-scripts.config.js
+++ clean-scripts.config.js
@@ -6,19 +6,22 @@ const excludeTsFiles = `"packages/@(core|vue|react|react-native|angular)/@(src|d
 
 const vueTemplateCommand = `file2variable-cli --config packages/vue/src/file2variable.config.js`
 const angularTemplateCommand = `file2variable-cli packages/angular/src/*.template.html -o packages/angular/src/variables.ts --html-minify --base packages/angular/src`
-const ngcSrcCommand = [
-  `ngc -p packages/core/src`,
-  `tsc -p packages/vue/src`,
-  `tsc -p packages/react/src`,
-  `ngc -p packages/angular/src`,
-  `tsc -p packages/react-native/src`
-]
-const tscDemoCommand = [
-  `tsc -p packages/vue/demo`,
-  `tsc -p packages/react/demo`,
-  `ngc -p packages/angular/demo`
-]
-const webpackCommand = `webpack`
+
+const tscCoreSrcCommand = `ngc -p packages/core/src`
+const tscVueSrcCommand = `tsc -p packages/vue/src`
+const tscReactSrcCommand = `tsc -p packages/react/src`
+const tscAngularSrcCommand = `ngc -p packages/angular/src`
+const tscReactNativeSrcCommand = `tsc -p packages/react-native/src`
+
+const tscVueDemoCommand = `tsc -p packages/vue/demo`
+const tscReactDemoCommand = `tsc -p packages/react/demo`
+const tscAngularDemoCommand = `ngc -p packages/angular/demo`
+
+const webpackVueCommand = `webpack --config packages/vue/demo/webpack.config.js`
+const webpackReactCommand = `webpack --config packages/react/demo/webpack.config.js`
+const webpackAngularJitCommand = `webpack --config packages/angular/demo/jit/webpack.config.js`
+const webpackAngularAotCommand = `webpack --config packages/angular/demo/aot/webpack.config.js`
+
 const revStaticCommand = `rev-static`
 const cssCommand = `cleancss ./node_modules/github-fork-ribbon-css/gh-fork-ribbon.css -o packages/core/demo/index.bundle.css`
 
@@ -26,11 +29,30 @@ module.exports = {
   build: [
     {
       js: [
-        vueTemplateCommand,
-        angularTemplateCommand,
-        ngcSrcCommand,
-        tscDemoCommand,
-        webpackCommand
+        tscCoreSrcCommand,
+        {
+          vue: [
+            vueTemplateCommand,
+            tscVueSrcCommand,
+            tscVueDemoCommand,
+            webpackVueCommand
+          ],
+          react: [
+            tscReactSrcCommand,
+            tscReactDemoCommand,
+            webpackReactCommand
+          ],
+          angular: [
+            angularTemplateCommand,
+            tscAngularSrcCommand,
+            tscAngularDemoCommand,
+            {
+              webpackAngularJitCommand,
+              webpackAngularAotCommand
+            }
+          ],
+          reactNative: tscReactNativeSrcCommand
+        }
       ],
       css: cssCommand,
       clean: `rimraf "packages/@(core|vue|react|angular)/demo/**/@(*.bundle-*.js|*.bundle-*.css)"`
@@ -54,11 +76,20 @@ module.exports = {
     js: `standard --fix ${jsFiles}`
   },
   watch: {
-    vue: `${vueTemplateCommand} --watch`,
-    angular: `${angularTemplateCommand} --watch`,
-    src: `${ngcSrcCommand} --watch`,
-    demo: `${tscDemoCommand} --watch`,
-    webpack: `${webpackCommand} --watch`,
+    vueTemplateCommand: `${vueTemplateCommand} --watch`,
+    angularTemplateCommand: `${angularTemplateCommand} --watch`,
+    tscCoreSrcCommand: `${tscCoreSrcCommand} --watch`,
+    tscVueSrcCommand: `${tscVueSrcCommand} --watch`,
+    tscReactSrcCommand: `${tscReactSrcCommand} --watch`,
+    tscAngularSrcCommand: `${tscAngularSrcCommand} --watch`,
+    tscReactNativeSrcCommand: `${tscReactNativeSrcCommand} --watch`,
+    tscVueDemoCommand: `${tscVueDemoCommand} --watch`,
+    tscReactDemoCommand: `${tscReactDemoCommand} --watch`,
+    tscAngularDemoCommand: `${tscAngularDemoCommand} --watch`,
+    webpackVueCommand: `${webpackVueCommand} --watch`,
+    webpackReactCommand: `${webpackReactCommand} --watch`,
+    webpackAngularJitCommand: `${webpackAngularJitCommand} --watch`,
+    webpackAngularAotCommand: `${webpackAngularAotCommand} --watch`,
     rev: `${revStaticCommand} --watch`
   },
   screenshot: [
