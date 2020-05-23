762380a186f9fc9d27c4fd05b61c01cfc68dbccd
York Yao <me@yorkyao.com>
2020-05-23 08:48:20 +0800

    chore: update dependencies

--- packages/react/demo/index.tsx
+++ packages/react/demo/index.tsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import { RelativeTime } from '../dist/'
 
-class Main extends React.Component<{}, {}> {
+class Main extends React.Component<unknown, unknown> {
   private locale = null
 
   private time1 = Date.now() - 1000 * 60 * 60 * 24 * 365 * 5
